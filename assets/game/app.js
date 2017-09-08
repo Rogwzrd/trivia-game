0a96efaa88690fa6b9af15680edd1edfb7180f36
unknown <mgdaye@gmail.com>
2017-09-08 18:39:59 -0400

    fixed some spelling errors

--- assets/game/app.js
+++ assets/game/app.js
@@ -155,8 +155,8 @@ $(document).ready(function() {
     function gameEndDisplayPage() {
         gameContainer.html("<h1> Thanks For Playing")
         questionContainer.html("<p>Here are your result!<p>" + "<p>You answered " + rightAnswers + " qustions correctly!</p>" +
-            "<p>You asnwered " + wrongAnswers + " incorrectly!</p>" +
-            "<p>You didn't answer" + unanswered + "qustions!</p>");
+            "<p>You answered " + wrongAnswers + " incorrectly!</p>" +
+            "<p>" + unanswered + " quetions went unanswered!</p>");
         answerContainer.html("<h2>Hang around to play again!</h2>");
     }
 
