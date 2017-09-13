443ec8eb41709ce905427529df1e9a261aebf274
unknown <mgdaye@gmail.com>
2017-09-12 20:51:59 -0400

    added audio for wins/losses/right answers/wrong answers

--- assets/game/app.js
+++ assets/game/app.js
@@ -2,67 +2,75 @@ $(document).ready(function() {
 
     var questionBank = [
 
-        {
-            game: "Devil May Cry",
-            question: "What are the names of the protagonist Dante's handguns?",
-            answers: {
-                a: "Bert & Ernie",
-                b: "Crime & Punishment",
-                c: "Ebony & Ivory",
-                d: "Hall & Oates"
+            {
+                game: "Devil May Cry",
+                question: "What are the names of the protagonist Dante's handguns?",
+                answers: {
+                    a: "Bert & Ernie",
+                    b: "Crime & Punishment",
+                    c: "Ebony & Ivory",
+                    d: "Hall & Oates"
+                },
+                correctAnswer: ["c", "Ebony & Ivory"],
+                gif: "assets/images/devil-may-cry-gif.gif"
             },
-            correctAnswer: ["c", "Ebony & Ivory"],
-            gif: "assets/images/devil-may-cry-gif.gif"
-        },
-        {
-            game: "Mega Man",
-            question: "Which of the following bosses did not appear in Megaman 2 for the NES?",
-            answers: {
-                a: "Metal Man",
-                b: "Flash Man",
-                c: "Wood Man",
-                d: "Cuts Man"
+            {
+                game: "Mega Man",
+                question: "Which of the following bosses did not appear in Megaman 2 for the NES?",
+                answers: {
+                    a: "Metal Man",
+                    b: "Flash Man",
+                    c: "Wood Man",
+                    d: "Cuts Man"
+                },
+                correctAnswer: ["d", "Cuts Man"],
+                gif: "assets/images/cuts-man-gif.gif"
             },
-            correctAnswer: ["d", "Cuts Man"],
-            gif: "assets/images/cuts-man-gif.gif"
-        },
-        {
-            game: "Residen Evil",
-            question: "What is the name of the original virus that turned people into Zombies in Resident Evil?",
-            answers: {
-                a: "T-Virus",
-                b: "Progenitor Virus",
-                c: "G-Virus",
-                d: "Demon's Blood"
+            {
+                game: "Residen Evil",
+                question: "What is the name of the original virus that turned people into Zombies in Resident Evil?",
+                answers: {
+                    a: "T-Virus",
+                    b: "Progenitor Virus",
+                    c: "G-Virus",
+                    d: "Demon's Blood"
+                },
+                correctAnswer: ["a", "T-Virus"],
+                gif: "assets/images/resident-evil-gif.gif"
             },
-            correctAnswer: ["a", "T-Virus"],
-            gif: "assets/images/resident-evil-gif.gif"
-        },
-        {
-            game: "Marve Vs. Capcom",
-            question: "Which of the following characters has never appeared in the Marvel Vs Capcom series?",
-            answers: {
-                a: "Rolento",
-                b: "Cable",
-                c: "Blackheart",
-                d: "Nemesis"
+            {
+                game: "Marve Vs. Capcom",
+                question: "Which of the following characters has never appeared in the Marvel Vs Capcom series?",
+                answers: {
+                    a: "Rolento",
+                    b: "Cable",
+                    c: "Blackheart",
+                    d: "Nemesis"
+                },
+                correctAnswer: ["a", "Rolento"],
+                gif: "assets/images/rolento-gif.gif"
             },
-            correctAnswer: ["a", "Rolento"],
-            gif: "assets/images/rolento-gif.gif"
-        },
-        {
-            game: "Street Fighter",
-            question: "In the japanese version of Street Fighter 2, what is M. Bison's name?",
-            answers: {
-                a: "Balrog",
-                b: "Derek",
-                c: "F. Bison",
-                d: "Vega"
-            },
-            correctAnswer: ["d", "Vega"],
-            gif: "assets/images/bison-gif.gif"
-        }
-    ];
+            {
+                game: "Street Fighter",
+                question: "In the japanese version of Street Fighter 2, what is M. Bison's name?",
+                answers: {
+                    a: "Balrog",
+                    b: "Derek",
+                    c: "F. Bison",
+                    d: "Vega"
+                },
+                correctAnswer: ["d", "Vega"],
+                gif: "assets/images/bison-gif.gif"
+            }
+        ],
+
+        chunLaugh = new Audio("assets/audio/chunLiLaugh.wav"),
+        sagatLaugh = new Audio("assets/audio/sagatLaugh.wav"),
+        scream = new Audio("assets/audio/chunLiScream.wav"),
+        cheer = new Audio("assets/audio/crowdCheer.wav"),
+        honda = new Audio("assets/audio/eHondaHeadbuttSound.wav"),
+        musicloss = new Audio("assets/audio/gameLossSound.wav"),
+        groanLoss = new Audio("assets/audio/genericLossGroan.wav");
 
     //these variables will hold the plaace of our questions, answers, and current game reference
     var mainContainer = $("#main-game"),
@@ -80,7 +88,6 @@ $(document).ready(function() {
         rightAnswers = 0,
         wrongAnswers = 0,
         unanswered = 0;
-
 
     //this resets the question timer
     function questionTimerReset() {
@@ -148,6 +155,7 @@ $(document).ready(function() {
         answerDisplayTimeoutId = setTimeout(function() {
             loadQuestion(currentLevel);
         }, 5000);
+        honda.play();
     }
     //this line holds the code for displaying the answer page based on an incorrect answer or timeout
     function incorrectAnswerDisplayPage() {
@@ -155,6 +163,7 @@ $(document).ready(function() {
         answerDisplayTimeoutId = setTimeout(function() {
             loadQuestion(currentLevel);
         }, 5000);
+        sagatLaugh.play();
     }
 
     function gameEndDisplayPage() {
@@ -163,7 +172,17 @@ $(document).ready(function() {
             "<p>You answered " + wrongAnswers + " incorrectly!</p>" +
             "<p>" + unanswered + " questions went unanswered!</p>");
         answerContainer.empty().append($("<button>").addClass("try-again btn btn-lg btn-primary").text("Try Again?"));
+        endGameAudio();
     }
+
+    function endGameAudio() {
+        if (rightAnswers == 5) {
+            cheer.play();
+        } else {
+            groanLoss.play();
+        }
+    }
+
 
     //The start button initiates the game
     $(".start-button").on("click", function() {
