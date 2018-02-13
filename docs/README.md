8db887d59c2a80da629ddb07c7c3f4c21a5590fd
Michael Daye <mgdaye@gmail.com>
2018-02-13 15:56:03 -0500

    added docs file

---
+++ docs/README.md
@@ -0,0 +1,15 @@
+# trivia-game
+
+The goal of this project was to build a trivia style game that utilizes timers and click events to manipulated the sreen and react to the user's inputs. I particularly enjoyed this project because of my investment into my chosen theme. I a huge of Capcom(the Japanese video game company, well known for creating the franchises Street Fighter, Reseident Evil, and Mega Man) games and being able to align my enthusiasm with learning and my love of Capcom's history was a huge boost to my productivity.
+
+***Skills I employed:***
+
+***Dom Manipulation with JavaScript:*** I really got to dive deep into dom manipulation with this project. Many of the aspects that happen on screen at being created dynamically within the code. The questions that produce, the musice that plays, the sound effects associated with correct and incorrect answers all happen based on the users input.
+
+***Timers:*** Learning how timers can utilized to run functions gave me a ton of creative control with this project. I was able to write a story with which the user interacts. The page layout adapting to the user response or lack of response allows them to become more immersed in the content of the page leading to a much more fulfilling experience.
+
+***jQuery:*** Using jQuery greatly expediated my ability to create and move elements in the DOM. This also allowed me to easily access and play music during transitions between states of the game(having the old school music adds a great touch to the presentation of the game).
+
+***Objects:*** This was my first huge success using objects to hold and manipulate data. I was able to hold and reproduce all of the the questions, answers, correct answers, and gifs. This made representing each question extremely easy because I only had to creat one function that would loop through the object holding the question data. In the even I want to add more questions to the game all I need to do is add the necessary data to the questions object and the rest of the code knnows what to do.
+
+Give the game a shot! 
---
+++ docs/assets/game/app.js
@@ -0,0 +1,247 @@
+$(document).ready(function() {
+
+    var questionBank = [
+
+            {
+                game: "Devil May Cry",
+                question: "What are the names of the protagonist Dante's handguns?",
+                answers: {
+                    a: "Bert & Ernie",
+                    b: "Crime & Punishment",
+                    c: "Ebony & Ivory",
+                    d: "Hall & Oates"
+                },
+                correctAnswer: ["c", "Ebony & Ivory"],
+                gif: "assets/images/devil-may-cry-gif.gif"
+            },
+            {
+                game: "Mega Man",
+                question: "Which of the following bosses did not appear in Megaman 2 for the NES?",
+                answers: {
+                    a: "Metal Man",
+                    b: "Flash Man",
+                    c: "Wood Man",
+                    d: "Cuts Man"
+                },
+                correctAnswer: ["d", "Cuts Man"],
+                gif: "assets/images/cuts-man-gif.gif"
+            },
+            {
+                game: "Residen Evil",
+                question: "What is the name of the original virus that turned people into Zombies in Resident Evil?",
+                answers: {
+                    a: "T-Virus",
+                    b: "Progenitor Virus",
+                    c: "G-Virus",
+                    d: "Demon's Blood"
+                },
+                correctAnswer: ["a", "T-Virus"],
+                gif: "assets/images/resident-evil-gif.gif"
+            },
+            {
+                game: "Marve Vs. Capcom",
+                question: "Which of the following characters has never appeared in the Marvel Vs Capcom series?",
+                answers: {
+                    a: "Rolento",
+                    b: "Cable",
+                    c: "Blackheart",
+                    d: "Nemesis"
+                },
+                correctAnswer: ["a", "Rolento"],
+                gif: "assets/images/rolento-gif.gif"
+            },
+            {
+                game: "Street Fighter",
+                question: "In the japanese version of Street Fighter 2, what is M. Bison's name?",
+                answers: {
+                    a: "Balrog",
+                    b: "Derek",
+                    c: "F. Bison",
+                    d: "Vega"
+                },
+                correctAnswer: ["d", "Vega"],
+                gif: "assets/images/bison-gif.gif"
+            }
+        ],
+
+        //game music sources
+        gameBgMusic = new Audio("assets/audio/gameBgMusic.mp3"),
+        gameStartMusic = new Audio("assets/audio/gameStartMusic.mp3"),
+        chunLaugh = new Audio("assets/audio/chunLiLaugh.wav"),
+        sagatLaugh = new Audio("assets/audio/sagatLaugh.wav"),
+        cheer = new Audio("assets/audio/crowdCheer.wav"),
+        musicloss = new Audio("assets/audio/gameLossSound.wav"),
+        groanLoss = new Audio("assets/audio/genericLossGroan.wav"),
+
+        //these variables will hold the plaace of our questions, answers, and current game reference
+        mainContainer = $("#main-game"),
+        questionContainer = $("#question-span"),
+        answerContainer = $("#answer-span"),
+        gameContainer = $("#game-span"),
+
+        //this variable will hold the time remaining 
+        timeRemaining = 30,
+
+        //this variable holds the current level
+        currentLevel = 0,
+
+        //these variables will hold the right, wrong, and unanswered variables
+        rightAnswers = 0,
+        wrongAnswers = 0,
+        unanswered = 0;
+
+    //this resets the question timer
+    function questionTimerReset() {
+        timeRemaining = 30;
+        clearInterval(questionIntervalId);
+    }
+
+    //this manages the visuals of the timer
+    function timerFunction() {
+        timeRemaining--;
+        $(".timer").html("<div class='timer'>Time remaining: " + timeRemaining + "</div>");
+        if (timeRemaining < 0) {
+            incorrectAnswerDisplayPage();
+            unanswered++;
+            currentLevel++;
+            questionTimerReset();
+        }
+    }
+
+    //this function resets the visual of the game output;
+    //it hides the start game button and shows the game;
+    //when you win it does the revese
+    function gameStart() {
+        $(".game-space").toggleClass("display-none");
+        $(".start-button").toggleClass("display-none");
+        currentLevel = 0;
+        loadQuestion(currentLevel);
+    }
+
+    function restartGame() {
+        rightAnswers = 0;
+        wrongAnswers = 0;
+        unanswered = 0;
+        currentLevel = 0;
+        loadQuestion(currentLevel);
+    }
+
+    //this test function will load the data from the questionBank to the page
+    function loadQuestion(x) {
+        if (x < questionBank.length) {
+            gameContainer.html("<h2>" + questionBank[x].game + "</h2>")
+            questionContainer.html("<h3>" + questionBank[x].question + "</h3>");
+            answerContainer.html(
+                "<div class='answer' value='a'>" + questionBank[x].answers.a + "</div>" +
+                "<div class='answer' value='b'>" + questionBank[x].answers.b + "</div>" +
+                "<div class='answer' value='c'>" + questionBank[x].answers.c + "</div>" +
+                "<div class='answer' value='d'>" + questionBank[x].answers.d + "</div>" +
+                "<div class='timer'>Time remaining: " + timeRemaining + "</div>"
+            );
+
+            //this variable will hold the setInterval
+            questionIntervalId = setInterval(function() {
+                timerFunction();
+            }, 1000);
+
+        } else {
+
+            gameEndDisplayPage();
+        }
+    }
+
+    //this line holds the code for displaying the answer page based on a correct answer
+    function correctAnswerDisplayPage() {
+        answerContainer.html("<h3>Correct! The answer was " + questionBank[currentLevel].correctAnswer[1] + "!</h3>" + "<img src=" + questionBank[currentLevel].gif + " height ='250'>");
+        answerDisplayTimeoutId = setTimeout(function() {
+            loadQuestion(currentLevel);
+        }, 5000);
+        chunLaugh.play();
+    }
+    //this line holds the code for displaying the answer page based on an incorrect answer or timeout
+    function incorrectAnswerDisplayPage() {
+        answerContainer.html("<h3> Sorry! The answer was " + questionBank[currentLevel].correctAnswer[1] + "!</h3>" + "<img src=" + questionBank[currentLevel].gif + " height ='250'>");
+        answerDisplayTimeoutId = setTimeout(function() {
+            loadQuestion(currentLevel);
+        }, 5000);
+        sagatLaugh.play();
+    }
+
+    function gameEndDisplayPage() {
+        gameContainer.html("<h1> Thanks For Playing")
+        questionContainer.html("<p>Here are your results:<p>" + "<p>You answered " + rightAnswers + " questions correctly!</p>" +
+            "<p>You answered " + wrongAnswers + " incorrectly!</p>" +
+            "<p>" + unanswered + " questions went unanswered!</p>");
+        answerContainer.empty().append($("<button>").addClass("try-again btn btn-lg btn-primary").text("Try Again?"));
+        endGameAudio();
+    }
+
+    //this determines the soundbyte that plays when you finish the game
+    function endGameAudio() {
+
+        if (rightAnswers == 5) {
+
+            cheer.play();
+
+        } else {
+
+            gameBgMusic.pause();
+            gameBgMusic.currentTime = 0;
+            musicloss.play();
+            groanLoss.play();
+        }
+    }
+
+    //background music function
+    function backgroundAudio() {
+
+
+        //when you press the start button pause the start music and loop the background music
+        $(document).on("click", ".btn", function() {
+            gameStartMusic.pause();
+            gameBgMusic.addEventListener("ended", function() {
+                this.currentTime = 0;
+                this.play();
+            }, false);
+            gameBgMusic.play();
+        })
+    }
+
+    //when the window loads play the start music
+    window.addEventListener("load", function() {
+        gameStartMusic.play();
+    });
+
+    //The start button initiates the game
+    $(".start-button").on("click", function() {
+        gameStart();
+        backgroundAudio();
+    })
+
+    //the restart button restarts the game
+    $(document).on("click", ".try-again", function() {
+        restartGame();
+        backgroundAudio();
+    })
+
+    //this determines whether or not an answer is correct onclick;
+    //this line compares the value of the correct answer property in the object representing the current level and the value of the div you click on
+    $(document).on("click", ".answer", function() {
+        var yourSelection = $(this).attr("value");
+
+        if ((yourSelection == questionBank[currentLevel].correctAnswer[0]) && (currentLevel < questionBank.length)) {
+
+            rightAnswers++;
+            correctAnswerDisplayPage();
+            questionTimerReset();
+            currentLevel++;
+
+        } else {
+
+            wrongAnswers++;
+            incorrectAnswerDisplayPage();
+            questionTimerReset();
+            currentLevel++;
+        }
+    })
+})
